a27cc4ba80c2bb92765e4a44253dd2cc7c890c90
Youssef Ahmed <joetifa2003@gmail.com>
2020-10-11 18:00:40 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 const autoprefixer = require("autoprefixer")();
 const tailwindcss = require("tailwindcss")("./tailwind.config.js");
-const flexGapPolyfill = require("flex-gap-polyfill");
 
 module.exports = {
-  plugins: [tailwindcss, autoprefixer, flexGapPolyfill],
+  plugins: [tailwindcss, autoprefixer],
 };
